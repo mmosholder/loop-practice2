e3ae294509b66298540fb5e78c43d505d071e478
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-13 12:28:21 -0600

    project exercises started, perhaps complete

---
+++ main.js
@@ -0,0 +1,32 @@
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+// for (var i = 0; i < animals.length; i++) {
+// 	if (i !== animals.length -1) {
+// 		console.log(animals[i]);
+// 	}
+// }
+
+// 2nd
+
+// for (var i = 0; i < animals.length; i = i+2) {
+// 		console.log(animals[i]);
+
+// }
+
+// 3rd
+
+// for (var i = animals.length; i >= 0; i = i -1) {
+// 	console.log(animals[i]);
+// }
+
+
+//  4th
+for (var i = 0; i < animals.length; i++) {
+	if (i === animals.length - animals.length || i === animals.length - 1) {
+		console.log(animals[i]);
+	}
+	else {
+		console.log(animals[i]);
+		console.log(animals[i]);
+	}
+}
